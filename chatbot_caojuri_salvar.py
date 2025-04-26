65069fafd25490ea70af65d20ecb8b2126d16027
jonhsel <jonhselmo.engcomp@gmail.com>
2025-04-26 17:11:32 -0300

    teste12

--- chatbot_caojuri_salvar.py
+++ chatbot_caojuri_salvar.py
@@ -173,7 +173,8 @@ def pagina_chat():
 def sidebar():
     tabs_assistente = st.tabs(['Seleção de Arquivos'])
     with tabs_assistente[0]:
-        pasta_arquivos = os.path.join('arquivos')
+        #pasta_arquivos = os.path.join('arquivos')
+        pasta_arquivos = os.path.join(os.getcwd(), 'arquivos')
         #pasta_arquivos = '/mount/src/chatbot_caojuri2/arquivos/'
 
         # Tente primeiro com o caminho completo para o GitHub
