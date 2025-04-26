d04a13730bf94fed8300be7acca87d27003abe2c
jonhsel <jonhselmo.engcomp@gmail.com>
2025-04-26 16:19:09 -0300

    arquivos

--- chatbot_caojuri_salvar.py
+++ chatbot_caojuri_salvar.py
@@ -169,7 +169,7 @@ def pagina_chat():
 def sidebar():
     tabs_assistente = st.tabs(['Seleção de Arquivos'])
     with tabs_assistente[0]:
-        pasta_arquivos = 'arquivos'  # Define a pasta de arquivos
+        pasta_arquivos = './arquivos'  # Define a pasta de arquivos
         if not os.path.exists(pasta_arquivos) or not os.listdir(pasta_arquivos):
             st.warning(f"Nenhum arquivo encontrado na pasta '{pasta_arquivos}'.")
             return  # Sai da função se não houver arquivos na pasta
