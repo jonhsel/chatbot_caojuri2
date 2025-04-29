1f72d7ee9904f8a4096acc783ba6185c0bab9aa7
jonhsel <jonhselmo.engcomp@gmail.com>
2025-04-29 15:24:42 -0300

    teste 2 hidden

--- chatbot_caojuri_salvar.py
+++ chatbot_caojuri_salvar.py
@@ -40,6 +40,20 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+hide_streamlit_style2 = """
+            <style>
+            /* Opcional: Oculta o rodapé 'Made with Streamlit' */
+            ._profilePreview_gzau3_63 {
+                display: none;
+                visibility: hidden;   
+            }
+            /* Opcional: Oculta a barra de decoração superior (onde o menu fica) */
+            /* Descomente a linha abaixo se quiser ocultar toda a barra cinza superior */
+            /* div[data-testid="stDecoration"] { visibility: hidden; display: none; } */
+            </style>
+            """
+st.markdown(hide_streamlit_style2, unsafe_allow_html=True)
+
 st.image('images/chatbot4.png')
 
 # Defina a API_KEY diretamente aqui
