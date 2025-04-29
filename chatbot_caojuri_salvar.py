612beb3c79a4a36b5f14de0ed809ec76b70e933f
jonhsel <jonhselmo.engcomp@gmail.com>
2025-04-29 15:05:10 -0300

    hidden avatar

--- chatbot_caojuri_salvar.py
+++ chatbot_caojuri_salvar.py
@@ -29,7 +29,7 @@ hide_streamlit_style = """
                 display: none; /* Garante que não ocupe espaço */
             }
             /* Opcional: Oculta o rodapé 'Made with Streamlit' */
-            div[class="_profilePreview_gzau3_53"] {
+            div[data-testid="appCreatorAvatar"] {
                 visibility: hidden;
                 display: none;
             }
